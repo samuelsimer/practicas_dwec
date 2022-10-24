e42aaf2f941e3098fb8b0484c242b295341d1a13
Samuel Remis <samuelremja@gmail.com>
2022-10-24 13:14:28 +0200

    Ejercicio 24 nuevo

---
+++ tema03/js/pr0324nuevo.js
@@ -0,0 +1,43 @@
+function addNif(array){    
+    array.forEach((item) => {
+        let letraDni = (item.dni%23) + 9;
+        letraDni = letraDni.toString(36).toUpperCase();//transformo el resto con base 36 a letra
+        let nif = item.dni + letraDni;        
+        console.log(letraDni.toString(36));
+        delete item.dni;
+        item['nif'] = nif;
+    });
+}
+
+let arr = [
+    {
+        nombre: 'Pepe',
+        ape1: 'Pérez',
+        ape2: 'López',
+        dni: '10100100',
+        expediente: '1720',
+        pass: '1234ABC',
+        ciclo: 'DAW',
+        notas: {
+            DWEC: 7.8,
+            DIW: 5.4,
+            DWEC: 9.4
+        }
+    },
+    {
+        nombre: 'Juan',
+        ape1: 'Mázquez',
+        ape2: 'Hernández',
+        dni: '7340831',
+        expediente: '342',
+        pass: 'P@ssw0rd',
+        ciclo: 'DAW',
+        notas: {
+            DWEC: 8,
+            DIW: 5.2,
+            DWES: 4
+        }
+    }
+]
+addNif(arr)
+console.log(arr);
